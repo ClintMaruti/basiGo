527316acec88759581189fc7743131e2de889d9c
Clint Maruti <clintmaruti@Clints-MacBook-Pro.local>
2022-08-10 22:19:37 +0300

    Finishes first part

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const server = express();
+
+const PORT = process.env.PORT || 4000;
+
+server.use(express.static("public"));
+
+server.get("/", (req, res) => {
+    res.render("index");
+});
+
+server.set("view engine", "ejs");
+
+server.get("/users/login", (req, res) => {
+    res.render("login");
+});
+
+server.get("/users/signup", (req, res) => {
+    res.render("signup");
+});
+
+server.get("/users/dashboard", (req, res) => {
+    res.render("dashboard", { user: "Clint" });
+});
+
+server.listen(PORT, () => {
+    console.debug(`⚡️[server]: Server is running at http://localhost:${PORT}`);
+});
