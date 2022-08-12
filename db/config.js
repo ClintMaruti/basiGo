44476fe8af57a03478c9ea8b92e372d3a7e88f2f
Clint Maruti <clintmaruti@Clints-MacBook-Pro.local>
2022-08-12 14:22:31 +0300

    added Database URL to connection string

--- db/config.js
+++ db/config.js
@@ -6,7 +6,7 @@ const isProduction = process.env.NODE_ENV === "production";
 
 // const connectionString = `postgresql://${process.env.DB_USER ?? "postgres"}:${process.env.DB_PASSWORD ?? "postgres"}@${process.env.DB_HOST ?? localhost}:${process.env.DB_PORT ?? 5432}/${process.env.DB_DATABASE ?? `basigo`}`;
 
-const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+const connectionString = process.env.DATABASE_URL;
 
 const pool = new Pool({
     connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
