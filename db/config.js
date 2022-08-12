a3011276277d70f5a76e174eec0e56400007fcce
Clint Maruti <clintmaruti@Clints-MacBook-Pro.local>
2022-08-12 16:35:53 +0300

    Did some code refactors

--- db/config.js
+++ db/config.js
@@ -7,21 +7,22 @@ const { Pool } = require("pg");
 
 const isProduction = process.env.NODE_ENV === "production";
 
-const connectionString = process.env.DATABASE_URL;
+// const connectionString = process.env.DATABASE_URL;
+
+const connectionString = `postgresql://${process.env.DB_USER ?? "postgres"}:${process.env.DB_PASSWORD ?? "postgres"}@${process.env.DB_HOST ?? localhost}:${process.env.DB_PORT ?? 5432}/${process.env.DB_DATABASE ?? `basigo`}`;
 
 const pool = new Pool({
     connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-    ssl: {
-        rejectUnauthorized: false,
-    },
+    // ssl: {
+    //     rejectUnauthorized: false,
+    // },
 });
 
-// pool.query(sql, (err, results) => {
-//     if (err) {
-//         throw err;
-//     }
-//     console.log("🚀  Database Updated Successfully!");
-//     process.exit(0);
-// });
+pool.query(sql, (err, results) => {
+    if (err) {
+        throw err;
+    }
+    console.log("🚀  Database Updated Successfully!");
+});
 
 module.exports = { pool };
