ed6d6bcd8a24f20f488252aa4041d885dfd26009
Clint Maruti <clintmaruti@Clints-MacBook-Pro.local>
2022-08-12 11:50:18 +0300

    Finishes implementing all features

--- server.js
+++ server.js
@@ -42,12 +42,35 @@ server.get("/users/login", checkAuthenticated, (req, res) => {
     res.render("login-page");
 });
 
-server.get("/users/dashboard", checkNotAuthenticated, (req, res) => {
-    res.render("dashboard", { user: req.user.user_name });
+// GET /dashboard
+server.get("/users/dashboard", checkNotAuthenticated, async (req, res) => {
+    // Fetch all leads and customers from the database if a lead is in the customer table
+    // remove it from the list to be displayed in the leads page
+    let error = [];
+    pool.query(`SELECT * FROM leads`, (err, results) => {
+        if (err) {
+            throw err;
+        }
+        if (results.rows.length > 0) {
+            if (req.user.isadmin) {
+                res.render("leads", { user: req.user.user_name, isAdmin: req.user.isadmin, leads: results.rows });
+            } else {
+                res.render("add-lead", { user: req.user.user_name, isAdmin: req.user.isadmin });
+            }
+        } else {
+            error.push({ message: "No Leads Available at the moment." });
+            req.flash("No leads available at the moment");
+            if (req.user.isadmin) {
+                res.render("leads", { user: req.user.user_name, isAdmin: req.user.isadmin, error: error });
+            } else {
+                res.render("add-lead", { user: req.user.user_name, isAdmin: req.user.isadmin });
+            }
+        }
+    });
 });
 
-// logout
-server.get("/users/logout", (req, res) => {
+// GET /logout
+server.get("/users/dashboard/logout", (req, res) => {
     req.logout((err) => {
         if (err) {
             return err;
@@ -57,6 +80,30 @@ server.get("/users/logout", (req, res) => {
     req.flash("success_msg", "You have logged out");
 });
 
+// GET /add leads
+server.get("/users/dashboard/add-leads", checkNotAuthenticated, (req, res) => {
+    res.render("add-lead", { user: req.user.user_name, isAdmin: req.user.isadmin });
+});
+
+// GET /add customer
+server.get("/users/dashboard/add-customer/:id", checkNotAuthenticated, async (req, res) => {
+    // fetch lead from the database
+    let error = [];
+    pool.query(`SELECT * FROM leads WHERE id = $1`, [req.params.id], (err, results) => {
+        if (err) {
+            throw err;
+        }
+        if (results.rows.length > 0) {
+            res.render("create-customer", { user: req.user.user_name, isAdmin: req.user.isadmin, lead: results.rows[0] });
+        } else {
+            error.push({ message: "No Leads Available at the moment." });
+            req.flash("No leads available at the moment");
+            res.render("create-customer", { user: req.user.user_name, isAdmin: req.user.isadmin, error: error });
+        }
+    });
+});
+
+// POST /login
 server.post(
     "/users/login",
     passport.authenticate("local", {
@@ -98,6 +145,49 @@ server.post(
         // console.log(email, password);
     }
 );
+
+// POST /Add Leads
+server.post("/users/dashboard/add-lead", async (req, res) => {
+    let { fName, sName, phone, location, gender, created_by } = req.body;
+
+    let errors = [];
+    // shallow validation check
+    if (!fName || !sName || !location || !gender || !created_by) {
+        errors.push({ message: "Make sure you fill all the missing fields" });
+    }
+
+    // query database
+    pool.query(`INSERT INTO leads (firstname, secondname, phonenumber, location, gender, createdby) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`, [fName, sName, phone, location, gender, created_by], (err, result) => {
+        if (err) {
+            throw err;
+        }
+
+        console.log(result.rows);
+        req.flash("success_msg", "You have successfully added leads into the database");
+        res.redirect("/users/dashboard/add-leads");
+    });
+});
+
+// POST /Create customer
+server.post("/users/dashboard/add-customer", async (req, res) => {
+    let { fName, sName, phone, location, gender, photo, earnings, product, created_by } = req.body;
+    let errors = [];
+    // shallow validation check
+    if (!fName || !sName || !location || !gender || !created_by || !earnings || !product) {
+        errors.push({ message: "Make sure you fill all the missing fields" });
+    }
+
+    // query database
+    pool.query(`INSERT INTO customer (firstname, secondname, phonenumber, location, gender, createdby, photo, earnings, product) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`, [fName, sName, phone, location, gender, created_by, photo, earnings, product], (err, result) => {
+        if (err) {
+            throw err;
+        }
+
+        console.log(result.rows);
+        req.flash("success_msg", "You have successfully added leads into the database");
+        res.redirect("/users/dashboard");
+    });
+});
 
 function checkAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
